74801c093ae3759e6565a7870d8db86c23f62be9
Rafał Majewski <goodheropl@gmail.com>
2022-06-11 08:25:46 +0200

    linear system size class

---
+++ src/lib/linearsystem/size/LinearSystemSize.hpp
@@ -0,0 +1,10 @@
+#ifndef LINEARSYSTEMSIZE_HPP_INCLUDED
+#define LINEARSYSTEMSIZE_HPP_INCLUDED
+
+
+struct LinearSystemSize {
+	const int equationsCount;
+	const int variablesCount;
+};
+
+#endif // LINEARSYSTEMSIZE_HPP_INCLUDED
