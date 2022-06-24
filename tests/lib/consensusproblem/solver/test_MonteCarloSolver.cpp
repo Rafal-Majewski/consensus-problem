e04abad0f0022f67f5406f01551fc0ed1de1b322
Rafał Majewski <goodheropl@gmail.com>
2022-06-24 10:11:51 +0200

    complex check 1

--- tests/lib/consensusproblem/solver/test_MonteCarloSolver.cpp
+++ tests/lib/consensusproblem/solver/test_MonteCarloSolver.cpp
@@ -68,3 +68,28 @@ TEST(MonteCarloSolver, sanity_check_3) {
 		0.1  // allow some error
 	);
 }
+
+TEST(MonteCarloSolver, complex_check_1) {
+	ConsensusProblem::MonteCarloSolver<double> solver(800000);
+
+	short statesCount = 3;
+	short* targetStates = new short[statesCount] {26, 0, 0};
+	short* startingStates = new short[statesCount] {10, 6, 10};
+	short rulesCount = 3;
+	ConsensusProblem::Rule* rules = new ConsensusProblem::Rule[rulesCount] {
+		{{0, 1}, {0, 0}},
+		{{2, 1}, {2, 2}},
+		{{0, 2}, {1, 1}}
+	};
+	EXPECT_NEAR(
+		solver.solve(
+			statesCount,
+			targetStates,
+			startingStates,
+			rulesCount,
+			rules
+		),
+		0.5,
+		0.05  // allow some error
+	);
+}
